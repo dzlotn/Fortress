1d22d64881565601057a52fd7725f01ca098fb46
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-03-25 10:43:55 -0400

    changed comments

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -2,12 +2,16 @@
 #The calculator then conducts the operation on the two numbers and returns the result
 
 def calcfunc(num1, op, num2):
+    #multiplaction func
     if op == "*":
         return num1*num2
+    #add func
     elif op == "+":
         return num1+num2
+    #sub func
     elif op=="-":
         return num1-num2
+    #division func
     elif op == "/":
         return float(num1/num2)
     
