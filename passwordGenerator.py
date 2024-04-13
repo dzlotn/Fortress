919b777493d3f068231141a37bc80188ffc24cfe
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-04-13 17:56:37 -0400

    added delete password file functionality and error catching

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -15,16 +15,15 @@ def rdmt1():
     check = True
     while True:
         num = random.randint(10,100)
-        print(f'\033[34mTrying Number: {num}\033[0m')
+        print(f'\n\033[34mTrying Number: {num}\033[0m')
         for j in range(2,num//2+1):
             if num%j == 0:
                 print(f"\033[31mNumber fails divisibility test of {j}\033[0m")
-                newprintLn()
                 check=False
                 break
         if check:
             print(f"\033[32mPrime Number Found: {num}\033[33m")
-            print(f"Generating Password ofn length {num}...\033[0m")
+            print(f"Generating Password of length {num}...\033[0m")
             newprintLn()
             return num
         else:
@@ -32,7 +31,6 @@ def rdmt1():
 #creates password using sets of characters and pwd length found by rdmt1                
 def rdmt2(num):
     #global variables
-    passwordlist = []
     letters = "abcdefghijklmnopqrstuvwxyz"
     numbers = "1234567890"
     statusDict = {
@@ -66,20 +64,26 @@ def rdmt2(num):
             break
             
     pwdf = "".join(pwd)
-    ans = input(f"\nComputer has created a secure password. Would you like to use: {pwdf} as one of your passwords? (yes or no) ").upper()
+    
+    ans = input(f"\nComputer has created a secure password. Would you like to use: \"{pwdf}\" as one of your passwords? (yes or no)\n").upper()
     if ans == "YES":
         # passwordlist.append(pwdf)
         # print(f'\n-------------\nFinal Password Generated:')
         # print(f"\033[32m{pwdf}\033[0m")
         pwdAppend1(pwdf)
+    elif ans=="NO":
+        rdmt2(rdmt1())
+    
     else:
-        rdmt2(num)  
+        print("Invalid Response. \033[033m Generating new password... \033[0m")
+        rdmt2(rdmt1())   
+               
     return pwdf
 
 def pwdAppend1(pwdf):
     prog = input("\nWhat program would you like to assign the password to? " )
     passwordManager[prog] = pwdf
-    print(f"Password({pwdf}) added to password manager under program ({prog})")
+    print(f"\033[32mPassword({pwdf}) added to password manager under program ({prog})\033[0m")
     
 def pwdAppend2():
     prog = input("\nWhat program would you like to assign the password to? " )
@@ -147,14 +151,18 @@ def pwdCheckKeyboardRows(pwd,num,statusDict):
                     print(f"\033[31mClose Characters on Keyboard Found (column wise): {charFst} and {charNxt}\n\033[0mPassword Status: {statusDict[False]}\033[33m\nGenerating New Password...\033[0m")
                     return False 
             
-            
+ 
+#prints passwords in nice table format           
 def printPasswords(passwordManager):
     print("\033[34m  {:<20}   {:<20}\033[0m".format("Program", "Password"))
     for program, password in passwordManager.items():
         print("  {:<20} | {:<15}".format(program,password))
         
-
-  
+#saves the passwords in a data.json file
+def savePasswords(passwordManager):
+    with open('data.json','w') as f:
+        json.dump(passwordManager,f) 
+        
 if __name__ == "__main__":
     if os.path.exists("data.json"):
         with open("data.json", "r") as p:
@@ -163,15 +171,19 @@ if __name__ == "__main__":
     else:
         passwordManager = {}
     while True:
-        options = input("\nWhat would you like to do? Type the number command. \n 1: Allow computer to create secure password \n 2: Store username and password \n 3: Delete an existing password from the manager \n 4: Reassign a password \n 5: Print all passwords \n 6: End Program\n Choice: ")
+        options = input("\nWhat would you like to do? Type the number command. \n 1: Allow computer to create secure password \n 2: Store username and password \n 3: Delete an existing password from the manager \n 4: Reassign a password \n 5: Print all passwords \n 6: Delete Password File (PERMANNENT) \n 7: End Program\n Choice: ")
         newprintLn()
         
+        #lets computer create new password
         if options =="1":
             rdmt2(rdmt1())
-            
+            savePasswords(passwordManager)
+        
+        #adds new password to manager  
         elif options == "2":
             pwdAppend2()
             
+        #runs delete password code
         elif options == "3":
             print("Here are the current passwords in the password manager: \n")
             printPasswords(passwordManager)
@@ -179,10 +191,12 @@ if __name__ == "__main__":
             if deletedSet in passwordManager: 
                 del passwordManager[deletedSet]
                 print(f"\033[31mPassword Deleted \033[0m")
+                savePasswords(passwordManager)
 
             else:
                 print(f"\033[31m\nPassword not found under written program\033[0m")
-                
+        
+        #allows for password reasignment     
         elif options =="4":
             print("\nHere are the current passwords in the password manager: ")
             printPasswords(passwordManager)
@@ -191,17 +205,26 @@ if __name__ == "__main__":
                 newPwd = input("\nType in the new password: ")
                 passwordManager[reasignedSet] = newPwd
                 print(f"\033[33mPassword Reasigned \033[0m")
+                savePasswords(passwordManager)
+
 
             else:
                 print(f"\033[31mPassword not found under written program\033[0m")
-                
+             
         elif options=="5":
             print("\nHere are the current passwords in the password manager: ")
             printPasswords(passwordManager)
-             
-        elif options == "6":
+        
+        #deletes password file
+        elif options =="6":
+            check = input("Are you want to delete the password file. This is a permament action! (YES or NO)\n").upper()
+            if check == "YES":
+                print(f"\033[31mPassword Data Removed\033[0m")
+                passwordManager = {}
+                os.remove("data.json")
+                
+        elif options == "7":
             print(f"\033[032mCode Completion - Passwords added to data.json file\033[0m")
             break
-        newprintLn()           
-        with open('data.json','w') as f:
-            json.dump(passwordManager,f)
+        else:
+            print("Invalid Command. Please try again")
