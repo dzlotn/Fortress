fc9c44908ba318731fb993556719d6d3b37cb2e0
dzlotn <75255812+dzlotn@users.noreply.github.com>
2024-04-14 11:32:07 -0400

    created pwd chance graph of amt over time

--- passwordGeneration/test.py
+++ passwordGeneration/test.py
@@ -6,18 +6,21 @@ amt_history = [amt]
 
 hit = False
 while not hit:
-    rand = random.choice([0.5, 0.5, 0.5, 1, 1, 1, 2, 2, 2]) 
+    rand = random.choice([0.33,0.5,0.5, 0.5, 0.5, 0.5,0.5,1, 1, 1,1.5, 1.5, 2, 2, 2,2,5]) 
     amt *= rand
     amt_history.append(amt)
 
     print(f"Round Number: {len(amt_history)}. Current amt = {amt_history[-1]}")
-    if amt >= 1000:
+    if amt >= 100000:
         print(f"Game won in {len(amt_history)} rounds")
         hit = True
+    if amt ==0:
+        print("Failure")
+        break
 
-# plt.plot(range(len(amt_history)), amt_history)
-# plt.xlabel('Number of Rounds')
-# plt.ylabel('Amount')
-# plt.title('Amount over Time')
-# plt.grid(True)
-# plt.show()
+plt.plot(range(len(amt_history)), amt_history)
+plt.xlabel('Number of Rounds')
+plt.ylabel('Amount')
+plt.title('Amount over Time')
+plt.grid(True)
+plt.show()
